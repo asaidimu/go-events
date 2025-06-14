8daa022a597ecf22c138185870aa75da2e1430da
Saidimu <47994458+asaidimu@users.noreply.github.com>
2025-06-14 10:04:48 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/asaidimu/go-events
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/asaidimu/go-events/pkg"
+)
+
+func main() {
+	fmt.Println(pkg.Greeting("World"))
+}
